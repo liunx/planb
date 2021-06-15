e7efb291cb1f5b3cff289e9e49e0f779631c6c43
Liunx <liunx163@163.com>
2021-06-15 18:48:03 +0800

    add more feature to finder.py.

--- finder.py
+++ finder.py
@@ -1,53 +1,53 @@
 import sys
 import cv2 as cv
+import numpy as np
+import random as rng
 import cvui
 from tools import histogram_calc
 from dnn_models import model_ssd_inception_v2_coco
+from aruco import ARUCO_DICT
+
+rng.seed(12345)
 
 
 class Detector:
-    def __init__(self, score_min=0.8):
-        self.id = 0
-        self.objects = {}
-        self.score_min = score_min
-        self.class_names, self.model = model_ssd_inception_v2_coco()
-
-    def histogram_compare(self, src, dst):
-        src = cv.resize(src, (300, 300))
-        dst = cv.resize(dst, (300, 300))
-        h1 = histogram_calc(src)
-        h2 = histogram_calc(dst)
-        return cv.compareHist(h1, h2, cv.HISTCMP_CORREL)
-
-    def store_object(self, frame, cname, score, box):
-        if score < self.score_min:
-            return
-        x, y, w, h = box
-        f = frame.copy()
-        crop_img = f[y:y+h, x:x+w]
-        found = False
-        for k, v in self.objects.items():
-            val = self.histogram_compare(v, crop_img)
-            if val > 0.6:
-                found = True
-                print(f'Found object {k}, val {val}!')
-                cv.imwrite(f'images/{k}.jpg', crop_img)
-                break
-        if not found:
-            k = f'{cname}{self.id}'
-            cv.imwrite(f'images/{k}.jpg', crop_img)
-            self.id += 1
-            self.objects[k] = crop_img
+    def __init__(self, min_scores=0.6):
+        self.min_scores = min_scores
 
     def draw_object(self, frame, box):
         cvui.rect(frame, box[0], box[1], box[2], box[3], 0xff0000)
 
-    def detect(self, frame, confidence=0.4, nms=0.4):
-        classes, scores, boxes = self.model.detect(frame, confidence, nms)
-        for (classid, score, box) in zip(classes, scores, boxes):
-            cname = self.class_names[classid[0]]
-            self.store_object(frame, cname, score, box)
-            self.draw_object(frame, box)
+    def process(self, frame, class_name, score, box):
+        if score < self.min_scores:
+            return
+        x, y, w, h = box
+        crop_frame = np.copy(frame[y:y+h, x:x+w])
+        corners = find_good_features(crop_frame)
+        draw_features(frame[y:y+h, x:x+w], corners)
+        self.draw_object(frame, box)
+
+
+def find_good_features(frame):
+    # Parameters for Shi-Tomasi algorithm
+    maxCorners = 13
+    qualityLevel = 0.01
+    minDistance = 10
+    blockSize = 3
+    gradientSize = 3
+    useHarrisDetector = False
+    k = 0.04
+    # Apply corner detection
+    src_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    corners = cv.goodFeaturesToTrack(src_gray, maxCorners, qualityLevel, minDistance, None,
+                                     blockSize=blockSize, gradientSize=gradientSize, useHarrisDetector=useHarrisDetector, k=k)
+    return corners
+
+
+def draw_features(frame, corners):
+    radius = 4
+    for i in range(corners.shape[0]):
+        cv.circle(frame, (int(corners[i, 0, 0]), int(corners[i, 0, 1])), radius, (rng.randint(
+            0, 256), rng.randint(0, 256), rng.randint(0, 256)), cv.FILLED)
 
 
 def main():
@@ -55,8 +55,10 @@ def main():
     vc = cv.VideoCapture(sys.argv[1])
     cv.namedWindow(win_name)
     cvui.init(win_name)
+    class_names, model = model_ssd_inception_v2_coco()
     detector = Detector()
-    last_histogram = []
+    aruco_dict = cv.aruco.Dictionary_get(ARUCO_DICT['DICT_4X4_50'])
+    aruco_params = cv.aruco.DetectorParameters_create()
     pause = False
     while True:
         if not pause:
@@ -65,8 +67,24 @@ def main():
                 break
             h, w, _ = frame.shape
             frame = cv.resize(frame, (int(w/2), int(h/2)))
-            detector.detect(frame)
-        _frame = frame.copy()
+            _frame = frame
+        else:
+            _frame = frame.copy()
+        # objects detecting
+        classes, scores, boxes = model.detect(_frame, 0.4, 0.4)
+        for (classid, score, box) in zip(classes, scores, boxes):
+            x, y, w, h = box
+            cname = class_names[classid[0]]
+            box_frame = frame[y:y+h, x:x+w]
+            # step 1: search aruco id
+            corners, ids, rejected = cv.aruco.detectMarkers(
+                box_frame, aruco_dict, parameters=aruco_params)
+            if len(corners) > 0:
+                for (markerCorner, markerID) in zip(corners, ids):
+                    pass
+
+            detector.process(_frame, cname, score, box)
+
         cvui.imshow(win_name, _frame)
         ch = cv.waitKey(1)
         if ch == ord('q'):
